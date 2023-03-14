2af014d0e8e6fc4433d879f19c98d862ec7fcd3f
Alba-SR <77280128+Alba-SR@users.noreply.github.com>
2023-03-14 20:16:14 +0100

    hay cosas que no se han metido en el commit prk jajas

---
+++ Assets/RoombaWorld/Spawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timerDust = 5;
+    public float timerMouse = Random.Range(20.0f,30.0f);
+    public GameObject mousePrefab;
+    public GameObject dustPrefab;
+    public RandomLocationGenerator randomLocationGenerator;
+    private GameObject[] exitPoints;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerDust -= Time.deltaTime;
+        timerMouse -= Time.deltaTime;
+
+        if (timerDust <= 0) 
+        {
+            //Instantiate(dustPrefab, RandomLocationGenerator.RandomWalkableLocation());
+            dustPrefab.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+            timerDust = 5;
+        }
+        if (timerMouse <= 0)
+        {
+            Instantiate(mousePrefab, RandomExitPoint().transform);
+            timerDust = Random.Range(20.0f, 30.0f);
+        }
+
+    }
+    public GameObject RandomExitPoint()
+    {
+        return exitPoints[Random.Range(0, exitPoints.Length)];
+    }
+}
